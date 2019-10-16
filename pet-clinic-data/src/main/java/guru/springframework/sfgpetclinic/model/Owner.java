c1cfec5cdccf8d6698e336247d6a598c706e6beb
Артур <artur.sviridov94@gmail.com>
2019-10-17 00:08:48 +0300

    Adding Bootstrap for Data. Closes #12

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -1,4 +1,11 @@
 package guru.springframework.sfgpetclinic.model;
 
 public class Owner extends Person {
+
+    public Owner() {
+    }
+
+    public Owner(String lastName) {
+        super.setLastName(lastName);
+    }
 }
